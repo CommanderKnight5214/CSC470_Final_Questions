77be0352b4ce70eaa0c3613ee3900b363ea01995
CommanderKnight5214 <CommanderKnight5214@users.noreply.github.com>
2021-12-16 16:18:25 -0600

    Updated Push

--- Final Questions/Question 6.rkt
+++ Final Questions/Question 6.rkt
@@ -1,15 +1,23 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname |Question 6|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-(define primeNumberPredicateHelper
-  (lambda (number counter number1 number2 identifier)
+(define primeNumber?
+  (lambda (number identifier counter)
     (cond
-      ((eq? number counter) "Your Number is not a Prime Number")
-      ((eq? identifier 1) "Your Number is a Prime Number")
-      ((eq? (% counter 2) 0) 
+      ((eq? identifier 2) "F")
+      ((eq? number counter) "T")
+      ((eq? (% number counter) 0) (primeNumberPredicateHelper number (+ identifier 1) (+ counter 1)))
+      (else (primeNumberPredicateHelper number identifier (+ counter 1))))))
+       
 
 
 
-(define primeNumberPredicate
+(define primeNumber?
   (lambda (number)
-    (primeNumberPredicateHelper number 0 0 0 1)
+    (primeNumberPredicateHelper number 0 0)
+
+
+
+
+;In Predicate Form, not a function
+(primeNumber? 3)
